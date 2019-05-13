cb1a211335a0d9df97c93aae1fd0db62834cc457
Jari Bakken <jari@faktisk.no>
2019-05-13 13:48:22 +0200

    config

--- config.rb
+++ config.rb
@@ -29,8 +29,8 @@ set :absolute_prefix, 'http://localhost:4567'
 # Build-specific configuration
 configure :build do
   puts "local build"
-  set :url_prefix, ""
-  set :absolute_prefix, ""
+  set :url_prefix, ENV['URL_PREFIX'] || ''
+  set :absolute_prefix, ENV['ABSOLUTE_PREFIX'] || ''
   activate :asset_hash
   activate :minify_javascript
   activate :minify_css
